61f5a532243257f2abbd9f94d114cf811b9be48c
Chukwunonso E. N <nonso4emma@yahoo.com>
2024-07-10 01:31:22 +0300

    Updated 1.0.4

--- v1/simple-tailwind-toast/example/src/App.tsx
+++ v1/simple-tailwind-toast/example/src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-// import { ExampleComponent } from 'simple-tailwind-toast'
 import SimpleToaster from 'simple-tailwind-toast'
 import 'simple-tailwind-toast/dist/index.css'
 
