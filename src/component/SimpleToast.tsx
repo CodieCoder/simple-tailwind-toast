63ee350b552917079a72315eddaf54a6501a5062
Chukwunonso E. N <nonso4emma@yahoo.com>
2024-07-14 23:36:25 +0300

    Working with tailwind
    Finally working with tailwindCSS

--- src/component/SimpleToast.tsx
+++ src/component/SimpleToast.tsx
@@ -1,12 +1,11 @@
-import React from "react"
-import { useSimpleToast } from "../store/hooks"
-import Toast from "./Toast"
-import "./styles.css"
-import { DEFAULT_CLASSES } from "../constants"
-import { ISimpleClassNames } from "../types"
+import React from 'react';
+import { useSimpleToast } from '../store/hooks';
+import Toast from './Toast';
+import { DEFAULT_CLASSES } from '../constants';
+import { ISimpleClassNames } from '../types';
 
 const SimpleToaster: React.FC<ISimpleClassNames> = (props) => {
-  const { store } = useSimpleToast()
+  const { store } = useSimpleToast();
 
   return (
     <div className={DEFAULT_CLASSES.simpleToast}>
@@ -14,7 +13,7 @@ const SimpleToaster: React.FC<ISimpleClassNames> = (props) => {
         <Toast key={toast.id} toast={toast} classNames={props} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default SimpleToaster
+export default SimpleToaster;
